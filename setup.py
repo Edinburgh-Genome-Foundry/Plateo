608815531a80c2e72a5c985e1716b9f2440127e2
Peter Vegh <pvegh@posteo.uk>
2020-09-29 13:51:13 +0100

    Travis CI fix

--- setup.py
+++ setup.py
@@ -1,19 +1,32 @@
 import ez_setup
+
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-exec(open('plateo/version.py').read()) # loads __version__
+exec(open("plateo/version.py").read())  # loads __version__
 
-setup(name='plateo',
-      version=__version__,
-      author='Zulko',
-    description='Read/write microplate and picklist data for lab automation',
-    long_description=open('README.rst').read(),
-    license='MIT',
+setup(
+    name="plateo",
+    version=__version__,
+    author="Zulko",
+    description="Read/write microplate and picklist data for lab automation",
+    long_description=open("README.rst").read(),
+    license="MIT",
     keywords="microplate biology parser converter report",
-    packages= find_packages(exclude='docs'),
+    packages=find_packages(exclude="docs"),
     include_package_data=True,
-    install_requires= ['pandas>=0.22', 'xlwt', 'xlrd', 'python-box', 'numpy',
-                       'matplotlib', 'tqdm', 'pdf_reports', 'flametree',
-                       'fuzzywuzzy', 'sequenticon'])
+    install_requires=[
+        "pandas>=0.22,<1.1",
+        "xlwt",
+        "xlrd",
+        "python-box",
+        "numpy",
+        "matplotlib",
+        "tqdm",
+        "pdf_reports",
+        "flametree",
+        "fuzzywuzzy",
+        "sequenticon",
+    ],
+)
