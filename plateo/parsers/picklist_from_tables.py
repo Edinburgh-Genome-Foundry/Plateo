edb1dd955dfe356fe57251f2fec15298a02e18a7
veghp <pvegh@posteo.uk>
2024-09-30 13:23:31 +0100

    Enable user-defined dataframe columns

--- plateo/parsers/picklist_from_tables.py
+++ plateo/parsers/picklist_from_tables.py
@@ -14,7 +14,9 @@ DF_COLUMNS = {
 }
 
 
-def picklist_from_dataframe(dataframe, source_plates, dest_plates):
+def picklist_from_dataframe(dataframe, source_plates, dest_plates, df_columns=None):
+    if df_columns == None:
+        df_columns = DF_COLUMNS
     # For matching the plates:
     source_plate_lookup = {plate.name: plate for plate in source_plates}
     dest_plate_lookup = {plate.name: plate for plate in dest_plates}
@@ -22,13 +24,13 @@ def picklist_from_dataframe(dataframe, source_plates, dest_plates):
     list_of_transfers = []
     for i, row in dataframe.iterrows():
         transfer = Transfer(
-            source_plate_lookup[row[DF_COLUMNS["source_plate"]]].wells[
-                row[DF_COLUMNS["source_well"]]
+            source_plate_lookup[row[df_columns["source_plate"]]].wells[
+                row[df_columns["source_well"]]
             ],
-            dest_plate_lookup[row[DF_COLUMNS["dest_plate"]]].wells[
-                row[DF_COLUMNS["dest_well"]]
+            dest_plate_lookup[row[df_columns["dest_plate"]]].wells[
+                row[df_columns["dest_well"]]
             ],
-            row[DF_COLUMNS["volume"]],
+            row[df_columns["volume"]],
         )
         list_of_transfers += [transfer]
 
@@ -38,37 +40,42 @@ def picklist_from_dataframe(dataframe, source_plates, dest_plates):
 
 
 def picklist_from_csv_file(
-    filename=None, unit=1e-6, source_plates="auto", dest_plates="auto"
+    filename=None, unit=1e-6, source_plates="auto", dest_plates="auto", df_columns=None
 ):
     # microliter = 1e-6
     # nanoliter = 1e-9
     dataframe = pandas.read_csv(filename)
+    if df_columns == None:
+        df_columns = DF_COLUMNS
 
     if source_plates == "auto":
-        source_plate_column = DF_COLUMNS["source_plate"]
+        source_plate_column = df_columns["source_plate"]
         source_plates = []
         for name in dataframe[source_plate_column].unique():
             nwells = infer_plate_size_from_wellnames(
                 dataframe[dataframe[source_plate_column] == name][
-                    DF_COLUMNS["source_well"]
+                    df_columns["source_well"]
                 ]
             )
             source_plate = get_plate_class(nwells)()
             source_plate.name = name
             source_plates += [source_plate]
     if dest_plates == "auto":
-        dest_plate_column = DF_COLUMNS["dest_plate"]
+        dest_plate_column = df_columns["dest_plate"]
         dest_plates = []
         for name in dataframe[dest_plate_column].unique():
             nwells = infer_plate_size_from_wellnames(
-                dataframe[dataframe[dest_plate_column] == name][DF_COLUMNS["dest_well"]]
+                dataframe[dataframe[dest_plate_column] == name][df_columns["dest_well"]]
             )
             dest_plate = get_plate_class(nwells)()
             dest_plate.name = name
             dest_plates += [dest_plate]
 
-    dataframe["volume"] = dataframe["volume"] * unit
+    dataframe[df_columns["volume"]] = dataframe[df_columns["volume"]] * unit
 
     return picklist_from_dataframe(
-        dataframe=dataframe, source_plates=source_plates, dest_plates=dest_plates
+        dataframe=dataframe,
+        source_plates=source_plates,
+        dest_plates=dest_plates,
+        df_columns=df_columns,
     )
--- tests/test_picklist_parsers.py
+++ tests/test_picklist_parsers.py
@@ -8,6 +8,9 @@ from plateo.parsers import (
 
 
 picklist_table_path = os.path.join("tests", "data", "parsers", "picklist_table.csv")
+picklist_table_path_columns = os.path.join(
+    "tests", "data", "parsers", "picklist_table_columns.csv"
+)
 
 
 def test_picklist_from_labcyte_echo_logfile():
@@ -25,4 +28,21 @@ def test_picklist_from_csv_file():
     assert (
         picklist.to_plain_string()
         == "Transfer 1.00E-06L from source A1 into destination B2"
+    )
+
+    # User-defined dataframe columns:
+    user_columns = {
+        "source_plate": "Source Plate",
+        "source_well": "Source Well",
+        "dest_plate": "Destination Plate",
+        "dest_well": "Destination Well",
+        "volume": "Volume",
+    }
+
+    picklist = picklist_from_csv_file(
+        picklist_table_path_columns, df_columns=user_columns
+    )
+    assert (
+        picklist.to_plain_string()
+        == "Transfer 3.00E-06L from source A8 into destination C4"
     )
