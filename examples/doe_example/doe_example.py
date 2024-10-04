5cc0599504b2a852c0785dafb0dceba7f6c8623a
veghp <pvegh@posteo.uk>
2024-10-04 12:01:23 +0100

    Export a platemap in the doe example

--- examples/doe_example/doe_example.py
+++ examples/doe_example/doe_example.py
@@ -1,8 +1,16 @@
 from plateo.parsers import plate_from_content_spreadsheet
-from plateo.applications.doe import (import_valuetable_from_csv, convert_valuetable_to_volumetable, convert_volumetable_to_actiontable)
+from plateo.applications.doe import (
+    import_valuetable_from_csv,
+    convert_valuetable_to_volumetable,
+    convert_volumetable_to_actiontable,
+)
 from plateo.containers import Plate96
 from plateo.parsers.picklist_from_tables import picklist_from_dataframe
-from plateo.exporters import (picklist_to_tecan_evo_picklist_file, plate_to_content_spreadsheet)
+from plateo.exporters import (
+    picklist_to_tecan_evo_picklist_file,
+    plate_to_content_spreadsheet,
+    plate_to_platemap_spreadsheet,
+)
 
 # READ THE SOURCE PLATE FROM A CONTENT SPREADSHEET
 source_plate = plate_from_content_spreadsheet("Source_Plate.xlsx")
@@ -24,7 +32,12 @@ actiontable = convert_volumetable_to_actiontable(volumetable, source_plate, dest
 actiontable.head()
 
 # CREATE PICKLIST THEN EXPORT IT AS A TECAN (GEMINI) WORKLIST
-picklist = picklist_from_dataframe(dataframe=actiontable, source_plates=[source_plate], dest_plates=[dest_plate], df_columns=None)
+picklist = picklist_from_dataframe(
+    dataframe=actiontable,
+    source_plates=[source_plate],
+    dest_plates=[dest_plate],
+    df_columns=None,
+)
 picklist_to_tecan_evo_picklist_file(picklist, "out_tecan_picklist.gwl")
 
 # SIMULATE THE PICKLIST
@@ -34,3 +47,18 @@ simulated_final_plate = simulated_plates[dest_plate]
 # EXPORT THE SIMULATED FINAL PLATE AS AN EXCEL SPREADSHEET
 plate_to_content_spreadsheet(simulated_final_plate, "out_final_plate.xlsx")
 # This records the contents, rather than the experimental units, for each well.
+
+
+# EXPORT A PLATEMAP OF EXPERIMENTAL UNITS
+def info_function(well):
+    if "expunit" in well.data:
+        return well.data["expunit"]
+    else:
+        return None
+
+
+plate_to_platemap_spreadsheet(
+    dest_plate,
+    wellinfo_function=info_function,
+    filepath="out_final_plate_expunits.xlsx",
+)
